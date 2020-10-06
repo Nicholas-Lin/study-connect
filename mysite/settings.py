2f7c7468436c6330bf09ebe8352202e80febb4e5
Nicholas Lin <nicholaslin10@gmail.com>
2020-10-05 22:03:27 -0400

    Finished login integration

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'social_app',
 
     'allauth', 
     'allauth.account',  
@@ -133,7 +134,7 @@ AUTHENTICATION_BACKENDS = (
  'allauth.account.auth_backends.AuthenticationBackend',
  )
 
-SITE_ID = 1
+SITE_ID = 2
 LOGIN_REDIRECT_URL = '/'
 
 SOCIALACCOUNT_PROVIDERS = {
