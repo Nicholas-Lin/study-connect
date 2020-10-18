b8bed9d146d3a72ecfe866a7af669866cccbd2c2
Nicholas Lin <nicholaslin10@gmail.com>
2020-10-18 17:55:09 -0400

    Added basic profile, added login alerts

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,10 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('login/', user_views.login, name='login'),
+    path('profile/', user_views.profile, name='profile'),
     path('', include('social_app.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
