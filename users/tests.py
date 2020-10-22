88d1e7f9b8e335e53165bf2964e756302976f363
Nicholas Lin <nicholaslin10@gmail.com>
2020-10-22 16:15:25 -0400

    Added test case

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from users.models import Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class ProfileTestCase(TestCase):
+    def test_profile_created(self):
+        new_user = User.objects.create_user(username="New_user", email="New_user@user.com")
+        self.assertEqual(new_user.username, "New_user")
+        self.assertEqual(new_user.email, "New_user@user.com")
+        self.assertTrue(isinstance(new_user.profile, Profile))
+    
