05f028fe2f92be79a9c5df8a4da49c93a5dae01a
Nicholas Lin <nicholaslin10@gmail.com>
2020-10-30 13:10:32 -0400

    Created discussion page, model, and basic view

---
+++ discussion/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'discussion/home.html', context)
--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'social_app.apps.SocialAppConfig',
+    'discussion.apps.DiscussionConfig',
     'users.apps.UsersConfig',
 
     'allauth', 
