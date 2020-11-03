e936907986aae7b6166eea5c5586c729b5de0e7a
Nicholas Lin <nicholaslin10@gmail.com>
2020-11-02 23:00:19 -0500

    Added student course and course editing form

--- social_app/admin.py
+++ social_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Course
+from .models import Course, StudentCourse
 
 # Register your models here.
 admin.site.register(Course)
+admin.site.register(StudentCourse)
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProfileDetailView
+from .views import ProfileDetailView, ProfileStudentCourseUpdate
 from .views import profile
 
 urlpatterns = [
     path('', profile, name='profile-edit'),
+    path('test/', ProfileStudentCourseUpdate.as_view(), name='profile-edit-courses'),
     path('<str:slug>/', ProfileDetailView.as_view(), name='profile-detail'),
 ]
