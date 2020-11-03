b9082ea77497388c6f1086683a1012c183b46af1
Nicholas Lin <nicholaslin10@gmail.com>
2020-11-03 09:15:15 -0500

    Changed courses url from "test" to "add"

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,6 @@ from .views import profile
 
 urlpatterns = [
     path('', profile, name='profile-edit'),
-    path('test/', ProfileStudentCourseUpdate.as_view(), name='profile-edit-courses'),
+    path('add/', ProfileStudentCourseUpdate.as_view(), name='profile-edit-courses'),
     path('<str:slug>/', ProfileDetailView.as_view(), name='profile-detail'),
 ]
