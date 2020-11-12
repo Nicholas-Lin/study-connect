6970371d4b874d5409fe5df7a8338a497d61d19a
Nicholas Lin <nicholaslin10@gmail.com>
2020-11-11 23:37:34 -0500

    Removed django filters

--- mysite/settings.py
+++ mysite/settings.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount',   
     'allauth.socialaccount.providers.google',
     'crispy_forms',
-    'django_filters',
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
