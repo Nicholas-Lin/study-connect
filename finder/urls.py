d11d43313da9d808ed0ff63cae2fe7a17a534622
Nicholas Lin <nicholaslin10@gmail.com>
2020-11-12 00:31:21 -0500

    Added find groups

--- finder/urls.py
+++ finder/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import profile_list
+from .views import profile_list, group_list, home
 
 urlpatterns = [
-    path('', profile_list, name='finder-home'),
+    path('', home, name='finder-home'),
+    path('buddy/', profile_list, name='finder-buddy'),
+    path('group/', group_list, name='finder-group'),
 ]
--- finder/views.py
+++ finder/views.py
@@ -3,11 +3,14 @@ from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
 from social_app.models import StudentCourse
 from users.models import Profile
+from groups.models import Group
 #from django_filters.views import FilterView
 #from .filters import ProfileFilter
 
+def home(request):
+    return render(request, "finder/home.html")
 
-def profile_list(request, template_name='finder/home.html'):
+def profile_list(request, template_name='finder/buddy.html'):
     course = None
     if request.GET.get('course'):
         course_filter = request.GET.get('course')
@@ -20,4 +23,16 @@ def profile_list(request, template_name='finder/home.html'):
         profiles = Profile.objects.all()
 
     context_dict = {'profiles': profiles, 'course': course}
-    return render(request, "finder/home.html", context_dict)
+    return render(request, "finder/buddy.html", context_dict)
+
+def group_list(request, template_name='finder/group.html'):
+    course = None
+    if request.GET.get('course'):
+        course_filter = request.GET.get('course')
+        course = StudentCourse.objects.get(id=course_filter)
+        groups = Group.objects.filter(course__subject=course.subject, course__catalog_number=course.catalog_number).exclude(private=True)
+    else:
+        groups = Group.objects.all().exclude(private=True)
+
+    context_dict = {'groups': groups, 'course': course}
+    return render(request, "finder/group.html", context_dict)
