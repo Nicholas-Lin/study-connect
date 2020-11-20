f316bbc8461042cbf51168c8e10d0a8e4cdc0aac
Nicholas Lin <nicholaslin10@gmail.com>
2020-11-19 22:39:43 -0500

    Edited course model for formset form

--- social_app/models.py
+++ social_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 from users.models import Profile
 
 # Create your models here.
@@ -9,9 +10,10 @@ class Course(models.Model):
 
 class StudentCourse(models.Model):
     # course = models.OneToOneField(Course, on_delete = models.CASCADE)
-    subject = models.CharField(max_length=4, blank=True)
-    catalog_number = models.CharField(max_length=4, blank=True)
-    difficulty = models.SmallIntegerField()
+    subject = models.CharField(max_length=4, blank=True, verbose_name="Subject (ex. CS)")
+    catalog_number = models.CharField(max_length=4, blank=True, verbose_name="Catalog Number (ex. 2150)")
+    difficulty = models.SmallIntegerField(
+        validators=[MaxValueValidator(10), MinValueValidator(1)], verbose_name="Difficulty from 1 (easy) to 10 (hard)")
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
 
