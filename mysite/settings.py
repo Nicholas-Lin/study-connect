d3e111dc6abc31bb07819c6c716ad66186c1022a
Nicholas Lin <nicholaslin10@gmail.com>
2020-11-22 15:14:20 -0500

    fixed scope in settings and group detail buttons

--- mysite/settings.py
+++ mysite/settings.py
@@ -157,7 +157,6 @@ SOCIALACCOUNT_PROVIDERS = {
         'SCOPE': [
             'profile',
             'email',
-            'calendar'
         ],
         'AUTH_PARAMS': {
             'access_type': 'online',
